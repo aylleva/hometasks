7f5eaa002ff83585ca279af9737f89d42622008e
ayleva <ayllayeva@gmail.com>
2024-10-03 00:55:12 +0400

    initial commit

---
+++ taskno2/taskno2/Program.cs
@@ -0,0 +1,16 @@
+﻿//task2
+Console.WriteLine("ededi daxil edin:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 2)
+{
+    Console.WriteLine("bu eded sade ededdir");
+}
+  else  if (n % 3 == 0 || n % 2 == 0 || n % 5 == 0)
+    {
+        Console.WriteLine("bu eded murekkeb ededdir");
+    }
+  else
+{
+    Console.WriteLine("bu eded sade ededdir");
+}
+   
---
+++ taskno3/taskno3/Program.cs
@@ -0,0 +1,13 @@
+﻿//task3
+
+Console.WriteLine("ededi daxil edin:");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 2; i < 1000; i*=2)
+ 
+
+    if (n == i)
+    {
+        Console.WriteLine("bu 2 quvvetlidir");
+    }
+
+
